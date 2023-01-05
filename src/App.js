b201104a1bc9665f216efbfd070453c5b96683d7
VictorN326 <21vnguyen3@gmail.com>
2023-01-05 08:01:05 -0800

    Fix App a bit and add css to fix background issue

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
-import React, { Component } from "react";
-import Navigation from "./Components/Navigation/Navigation";
-import Clarifai from "clarifai";
-import FaceRecognition from "./FaceRecognition/FaceRecognition";
-import ParticlesBg from "particles-bg";
-import SignInForm from "./Components/SignInForm/SignInForm";
-import Register from "./Components/Register/Register";
-import Logo from "./Components/Logo/Logo";
-import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
-import Rank from "./Components/Rank/Rank";
-import "./App.css";
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg'
+import Clarifai from 'clarifai';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import Navigation from './Components/Navigation/Navigation';
+import SignInForm from './Components/SignInForm/SignInForm';
+import Register from './Components/Register/Register';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import './App.css';
 
 const app = new Clarifai.App({
-  apiKey: "91ec176b33824469bc887e1bfac1ef66",
+ apiKey: '91ec176b33824469bc887e1bfac1ef66'
 });
-//This is to show the git push works
+
+
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: "",
-      imageUrl: "",
+      input: '',
+      imageUrl: '',
       box: {},
-      route: "SignInForm",
-      SignedIn: false
-    };
+      route: 'SigninForm',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById("inputimage");
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
     // Width and height will adjust and calculate to any value instead of giving
     //definite value
     const width = Number(image.width);
@@ -38,64 +56,87 @@ class App extends Component {
       //these calculated values will all go into the box state (displayFaceBox)
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
-    };
-  };
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
   displayFaceBox = (box) => {
-    console.log(box);
-    this.setState({ box: box });
-  };
+    this.setState({box: box});
+  }
+
   onInputChange = (event) => {
-    this.setState({ input: event.target.value });
-  };
+    this.setState({input: event.target.value});
+  }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000').then(response => response.json()).then(console.log);
+  // }
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
       //calculateFaceLocation returns an object. That object is needed in displayFaceBox function.
-      .then((response) =>
+    // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input) USED incase Clarifai server goes down
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        console.log('hi', response)
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
+        }
         this.displayFaceBox(this.calculateFaceLocation(response))
-      )
-      .catch((err) => console.log(err));
-  };
+      })
+      .catch(err => console.log(err));
+  }
 
   onRouteChange = (route) => {
-    if(route === 'SignOut') {
-      this.setState({SignedIn : false})
+    if (route === 'SignOut') {
+      this.setState({isSignedIn: false})
     } else if (route === 'home') {
-      this.setState({SignedIn: true})
+      this.setState({isSignedIn: true})
     }
-    this.setState( {route: route});
-  };
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <ParticlesBg className="pbg" type="cobweb" bg={true} />
-        <Navigation SignedIn = {this.state.SignedIn} onRouteChange={this.onRouteChange}/>
-        {this.state.route === "home" 
-        ?   <div>
-            <Logo />
-            <Rank />
-            <ImageLinkForm
-              onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
-            />
-            {
-              <FaceRecognition
-                box={this.state.box}
-                imageUrl={this.state.imageUrl}
+        <ParticlesBg type="fountain" bg={true} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries}
               />
-            }
-          </div>
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
           : (
-            this.state.route === 'SignInForm' ?
-              <SignInForm onRouteChange = {this.onRouteChange}/>
-            : <Register onRouteChange = {this.onRouteChange} />
-          )
+             route === 'SignInForm'
+             ? <SignInForm loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
         }
       </div>
     );
   }
 }
+
 export default App;
