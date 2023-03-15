b651155e8662a1eac8494d279435c680cc6e917f
VictorN326 <21vnguyen3@gmail.com>
2023-03-14 19:42:55 -0700

    changed some things but need lots of fixing now

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
-import ParticlesBg from 'particles-bg'
-import Clarifai from 'clarifai';
-import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
-import Navigation from './Components/Navigation/Navigation';
-import SignInForm from './Components/SignInForm/SignInForm';
-import Register from './Components/Register/Register';
-import Logo from './Components/Logo/Logo';
-import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
-import Rank from './Components/Rank/Rank';
-import './App.css';
+import React, { Component } from "react";
+import ParticlesBg from "particles-bg";
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
+import Navigation from "./Components/Navigation/Navigation";
+import SignInForm from "./Components/SignInForm/SignInForm";
+import Register from "./Components/Register/Register";
+import Logo from "./Components/Logo/Logo";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
+import "./App.css";
 
 // this is showing that git fetch works and I can get the updated code!
 // hello from the other side!
-const app = new Clarifai.App({
- apiKey: '91ec176b33824469bc887e1bfac1ef66'
-});
 
 class App extends Component {
   constructor() {
@@ -23,16 +19,16 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'SigninForm',
+      route: "SignInForm",
       isSignedIn: false,
       user: {
         id: '',
         name: '',
         email: '',
         entries: 0,
-        joined: ''
+        joined: '',
       }
-    }
+    };
   }
 
   loadUser = (data) => {
@@ -41,9 +37,10 @@ class App extends Component {
       name: data.name,
       email: data.email,
       entries: data.entries,
-      joined: data.joined
-    }})
-  }
+      joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -56,67 +53,73 @@ class App extends Component {
       //these calculated values will all go into the box state (displayFaceBox)
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   displayFaceBox = (box) => {
     this.setState({box: box});
   }
 
+
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
+    this.setState({ input: event.target.value });
+  };
 
   // componentDidMount() {
   //   fetch('http://localhost:3000').then(response => response.json()).then(console.log);
   // }
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(
-      //calculateFaceLocation returns an object. That object is needed in displayFaceBox function.
-    // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input) USED incase Clarifai server goes down
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      .then(response => {
-        console.log('hi', response)
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
-
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
       })
-      .catch(err => console.log(err));
-  }
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}))
+          })
+          .catch(console.log)
+
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
+  };
 
   onRouteChange = (route) => {
-    if (route === 'SignOut') {
-      this.setState({isSignedIn: false})
-    } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+    if (route === "SignOut") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route});
-  }
+    this.setState({ route: route });
+  };
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <ParticlesBg type="fountain" bg={true} />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        { route === 'home'
-          ? <div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}
+        />
+        {route === "home" 
+        ? (
+            <div>
               <Logo />
               <Rank
                 name={this.state.user.name}
@@ -128,12 +131,11 @@ class App extends Component {
               />
               <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
-          : (
-             route === 'SignInForm'
-             ? <SignInForm loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            )
-        }
+        ) : route === "SignInForm" ? (
+            <SignInForm loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+        ) : (
+            <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+        )}
       </div>
     );
   }
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ const Navigation = ({onRouteChange, SignedIn}) => {
          return(
             <nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
                 <p  onClick={() => onRouteChange('SignInForm')}  className = 'f3 link dim black underline pa3 pointer'>Sign In</p>
-                <p  onClick={() => onRouteChange('Register')}  className = 'f3 link dim black underline pa3 pointer'>Register</p>
+                <p  onClick={() => onRouteChange('register')}  className = 'f3 link dim black underline pa3 pointer'>Register</p>
             </nav>
 
             
